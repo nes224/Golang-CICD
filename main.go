a63166f94e6d4b63ebf2ae9170a7c803e8448c3a
nes224 <nes224@hotmail.com>
2024-02-05 14:23:28 +0700

    setup

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+	router := server.Group("/api")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": "dev"})
+	})
+}
